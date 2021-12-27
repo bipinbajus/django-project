d08c13eb87f170a0d46c416abbfdfeed60c7cdc9
bipinbajus <bipinbaj.us@gmail.com>
2021-12-27 05:37:07 +0000

    updates

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,17 +149,23 @@ def show_exam_result(request, course_id, submission_id):
     context = {}
     course = get_object_or_404(Course, pk=course_id)
     submission = get_object_or_404(Submission, pk=submission_id)
+    questions = Question.objects.filter(course=course)
     
     choices = submission.choices.all()
-    sum=0
+    points=0
+    totalGrade = 0
     for choice in choices:
         if(choice.is_correct):
-            sum += 1;
-            
-    print(sum)
+            points += 1
+    
+    for question in questions:
+        totalGrade += question.grade        
+    
+    score = int(points/totalGrade * 100)    
+
     context['course'] = course
     context['selected_ids'] = choices
-    context['grade'] = sum
+    context['grade'] = score
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context) 
            
            
